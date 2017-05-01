81b4d26a9257178f0887c6b59018deb3c9ec4450
Will Smith <will@toxon.co.uk>
2017-05-01 12:45:47 +0100

    WebSocket chat

---
+++ server/FactorioServer/Startup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace FactorioServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        private event EventHandler<(byte[], WebSocketReceiveResult)> MessageRecieved;
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            app.UseWebSockets();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        await Echo(context, webSocket);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+            });
+        }
+
+        private async Task Echo(HttpContext context, WebSocket webSocket)
+        {
+            async void OnMessageRecieved(object sender, (byte[], WebSocketReceiveResult) message)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(message.Item1), message.Item2.MessageType, message.Item2.EndOfMessage, CancellationToken.None);
+            }
+            MessageRecieved += OnMessageRecieved;
+
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                var message = new ArraySegment<byte>(buffer, 0, result.Count).ToArray();
+                MessageRecieved?.Invoke(this, (message, result));
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+
+            MessageRecieved -= OnMessageRecieved;
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+    }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,76 @@
 import React from 'react';
 
-const App = () => <h1>Hello World</h1>;
+class WebSocketFacade {
+  constructor(url) {
+    this.isConnected = false;
+    this.preconnectionBuffer = [];
 
-export default App;
+    this.ws = new WebSocket(url);
+    this.ws.addEventListener('open', (event) => {
+      console.log('Connection opened', event);
+
+      this.preconnectionBuffer.forEach(x => this.sendInternal(x));
+
+      this.isConnected = true;
+      this.preconnectionBuffer = [];
+    });
+    this.ws.addEventListener('closed', (event) => {
+      console.log('Connection closed', event);
+    });
+  }
+
+  send(data) {
+    if (this.isConnected) {
+      this.sendInternal(data);
+    } else {
+      this.preconnectionBuffer.push(data);
+    }
+  }
+
+  sendInternal(data) {
+    console.log('Sending message to server', event);
+    this.ws.send(data);
+  }
+
+  onMessage(handler) {
+    this.ws.addEventListener('message', handler);
+  }
+}
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: [],
+    };
+
+    this.ws = new WebSocketFacade('ws://toxon.co.uk:5000/ws');
+    this.ws.onMessage((data) => {
+      this.setState({
+        ...this.state,
+        messages: [
+          ...this.state.messages,
+          data,
+        ],
+      });
+    });
+  }
+
+  sendMessage() {
+    this.ws.send(this.messageInput.value);
+    this.messageInput.value = '';
+  }
+
+  render() {
+    return (<div>
+      <h1>Chat</h1>
+      <input ref={(input) => { this.messageInput = input; }} />
+      <button onClick={() => this.sendMessage()}>Send</button>
+      <ul>
+        {this.state.messages.map((message, idx) => <li key={idx}>{message.data}</li>)}
+      </ul>
+    </div>);
+  }
+}
